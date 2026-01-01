289bbdec5694aa7fca7b67b52175d1eaef53171e
Mohamed Elmarzougui <mohamed1elmarzougui@gmail.com>
2026-01-01 01:02:50 -0600

    Crete Moves collides with obstacles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,34 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.constraints = RigidbodyConstraints.FreezeRotationX |
+                         RigidbodyConstraints.FreezeRotationZ;
+                        // RigidbodyConstarints.FreezeRotationY;
+    }
+
+
 
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        
-        Vector3 move = new Vector3(x, 0f, z);
-        transform.Translate(move * speed * Time.deltaTime);
+
+        movement = new Vector3(x, 0f, z).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 newPosition = rb.position + movement * speed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
     }
 }
